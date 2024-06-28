3871d895ac55a6347afe08e5b6baf12e705707a0
Sergey Permyakov <serg.permyakov@gmail.com>
2024-06-28 15:58:28 -0700

    some scaleform stuff

--- CommonLibSF/include/RE/S/ScaleformGFxMovie.h
+++ CommonLibSF/include/RE/S/ScaleformGFxMovie.h
@@ -7,6 +7,39 @@ namespace RE::Scaleform::GFx
 {
 	class ASMovieRootBase;
 
+	class __declspec(novtable) Resource
+	{
+	public:
+		virtual ~Resource() = default;  // 00
+
+		// add
+		virtual void* GetKey();                       // 01
+		virtual std::uint32_t GetResourceTypeCode() const;  // 02
+		virtual void* GetResourceReport();        // 03
+
+		// members
+		uintptr_t refCount;  // 08
+		void* lib;              // 10
+	};
+
+	class __declspec(novtable) MovieDef :
+		public Resource,  // 00
+		public StateBag   // 18
+	{
+	public:
+
+		// add
+		virtual std::uint32_t GetVersion() const = 0;       // 04
+		virtual std::uint32_t GetLoadingFrame() const = 0;  // 05
+		virtual float         GetWidth() const = 0;         // 06
+		virtual float         GetHeight() const = 0;        // 07
+		virtual std::uint32_t GetFrameCount() const = 0;    // 08
+		virtual float         GetFrameRate() const = 0;     // 09
+		virtual std::uintptr_t GetFrameRect() const = 0;     // 0A
+		virtual std::uint32_t GetSWFFlags() const = 0;      // 0B
+		virtual const char*   GetFileURL() const = 0;
+	};
+
 	class Movie :
 		public RefCountBase<Movie, 327>,  // 00
 		public StateBag                   // 10
@@ -32,6 +65,7 @@ namespace RE::Scaleform::GFx
 		};
 
 		virtual ~Movie() = default;  // 01
+		virtual MovieDef* GetMovieDef() const = 0;                                                                                                                       // 01
 
 		// members
 		Ptr<ASMovieRootBase> asMovieRoot;
@@ -44,5 +78,6 @@ namespace RE::Scaleform::GFx
 		SF_RTTI_VTABLE(Scaleform__GFx__MovieImpl);
 
 		virtual ~MovieImpl() = default;  // 00
+		virtual MovieDef* GetMovieDef() const = 0;
 	};
 }
